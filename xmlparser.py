0bbd782121af5dc0bb28c2207ebb4b3280db0f58
Rex Nightingale <rex.nightingale@gmail.com>
2017-12-22 09:10:37 +0000

    Update xmlparser.py

--- xmlparser.py
+++ xmlparser.py
@@ -10,16 +10,7 @@ xmlfilename = 'heatmiserconfig.xml'
 # Will support the creation of 6 levels of data within the xml file
 def xmlupdate(thermostatID, setting, value, level2, level2name, level3, level3name, level4, level4name, level5, level5name):
     matchlevel = 0
-    
-    indentlevels = 5
-    if level5 == "":
-        indentlevels -= 1
-        if level4 == "":
-            indentlevels -= 1
-            if level3 == "":
-                indentlevels -= 1
-                if level2 == "":
-                    indentlevels -= 1
+    indentlevel = 0
     
     while matchlevel != 6:
         # Open files for processing
@@ -37,60 +28,61 @@ def xmlupdate(thermostatID, setting, value, level2, level2name, level3, level3na
             # Locate the right thermostatID
             if line.find("<thermostatID name=" + str(thermostatID) + ">") != -1:
                 matchlevel = 1
-            
-            if indentlevels >> 1:
-                if level2 != "":
-                    if matchlevel == 1:
-                        if line.find("<" + level2 + " name=" + level2name + ">") != -1:
-                            matchlevel = 2
-                        if line.find("</thermostatID>") != -1:
-                            space = ''
-                            space += ' ' * ((indentlevels - 4) * 4)
-                            filewrite.write(space + "<" + level2 + " name=" + level2name + ">\r\n")
-                            filewrite.write(space + "</" + level2 + ">\r\n")
+                indentlevel = 1
+                
+            if matchlevel == 1:
+                if level2:
+                    if line.find("<" + level2 + " name=" + level2name + ">") != -1:
+                        matchlevel = 2
+                        indentlevel = 2
+                    if line.find("</thermostatID>") != -1:
+                        space = ''
+                        space += ' ' * (indentlevel * 4)
+                        filewrite.write(space + "<" + level2 + " name=" + level2name + ">\r\n")
+                        filewrite.write(space + "</" + level2 + ">\r\n")
                 else:
                     return
-                
-            if indentlevels >> 2:
-                if level3 != "":
-                    if matchlevel == 2:
-                        if line.find("<" + level3 + " name=" + level3name + ">") != -1:
-                            matchlevel = 3
-                        if line.find("</" + level2 + ">") != -1:
-                            space = ''
-                            space += ' ' * ((indentlevels - 3) * 4)
-                            filewrite.write(space + "<" + level3 + " name=" + level3name + ">\r\n")
-                            filewrite.write(space + "</" + level3 + ">\r\n")
+
+            if matchlevel == 2:
+                if level3:
+                    if line.find("<" + level3 + " name=" + level3name + ">") != -1:
+                        matchlevel = 3
+                        indentlevel = 3
+                    if line.find("</" + level2 + ">") != -1:
+                        space = ''
+                        space += ' ' * (indentlevel * 4)
+                        filewrite.write(space + "<" + level3 + " name=" + level3name + ">\r\n")
+                        filewrite.write(space + "</" + level3 + ">\r\n")
                 else:
-                    return
-                
-            if indentlevels >> 3:
-                if level4 != "":
-                    if matchlevel == 3:
-                        if line.find("<" + level4 + " name=" + level4name + ">") != -1:
-                            matchlevel = 4
-                        if line.find("</" + level3 + ">") != -1:
-                            space = ''
-                            space += ' ' * ((indentlevels - 2) * 4)
-                            filewrite.write(space + "<" + level4 + " name=" + level4name + ">\r\n")
-                            filewrite.write(space + "</" + level4 + ">\r\n")
+                    matchlevel = 3
+
+            if matchlevel == 3:
+                if level4:
+                    if line.find("<" + level4 + " name=" + level4name + ">") != -1:
+                        matchlevel = 4
+                        indentlevel = 4
+                    if line.find("</" + level3 + ">") != -1:
+                        space = ''
+                        space += ' ' * (indentlevel * 4)
+                        filewrite.write(space + "<" + level4 + " name=" + level4name + ">\r\n")
+                        filewrite.write(space + "</" + level4 + ">\r\n")
                 else:
-                    return
-                
-            if indentlevels >> 4:
-                if level5 != "":
-                    if matchlevel == 4:
-                        if line.find("<" + level5 + " name=" + level5name + ">") != -1:
-                            matchlevel = 5
-                        if line.find("</" + level4 + ">") != -1:
-                            space = ''
-                            space += ' ' * ((indentlevels - 1) * 4)
-                            filewrite.write(space + "<" + level5 + " name=" + level5name + ">\r\n")
-                            filewrite.write(space + "</" + level5 + ">\r\n")
+                    matchlevel = 4
+
+            if matchlevel == 4:
+                if level5:
+                    if line.find("<" + level5 + " name=" + level5name + ">") != -1:
+                        matchlevel = 5
+                        indentlevel = 5
+                    if line.find("</" + level4 + ">") != -1:
+                        space = ''
+                        space += ' ' * (indentlevel * 4)
+                        filewrite.write(space + "<" + level5 + " name=" + level5name + ">\r\n")
+                        filewrite.write(space + "</" + level5 + ">\r\n")
                 else:
-                    return
-                
-            if setting != "" and value != "":
+                    matchlevel = 5
+
+            if setting and value:
                 if matchlevel == 5:
                     if line.find("<" + setting + ">" + value + "</" + setting + ">") != -1:
                         matchlevel = 6
@@ -98,18 +90,25 @@ def xmlupdate(thermostatID, setting, value, level2, level2name, level3, level3na
                         continue 
                     if line.find("<" + setting + ">") != -1:
                         space = ''
-                        space += ' ' * ((indentlevels - 0) * 4)
+                        space += ' ' * (indentlevel * 4)
                         filewrite.write(space + "<" + setting + ">" + value + "</" + setting + ">\r\n")
                         matchlevel = 6
                         continue
-                    if line.find("</" + level5 + ">") != -1:
+                    something = level5
+                    if not level5:
+                        something = level4
+                        if not level4:
+                            something = level3
+                            if not level3:
+                                something = level2
+                    if line.find("</" + something + ">") != -1:
                         space = ''
-                        space += ' ' * ((indentlevels - 0) * 4)
+                        space += ' ' * (indentlevel * 4)
                         filewrite.write(space + "<" + setting + ">" + value + "</" + setting + ">\r\n")
                         matchlevel = 6
             else:
                 return
-            
+
             filewrite.write(line)
             
         if matchlevel == 0:
